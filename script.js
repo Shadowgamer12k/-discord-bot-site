5fcd256cf4bc132c96ecfdacfcf6082e7cacabcb
vector! <123848980+Shadowgamer12k@users.noreply.github.com>
2025-03-18 23:36:09 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function sendMessage() {
-    const webhookURL = "YOUR_DISCORD_WEBHOOK_URL"; // ضع رابط Webhook الخاص بك هنا
+    const webhookURL = "https://discord.com/api/webhooks/1351700670075371651/tp3byHb85gA-A2uQ7NPbhSl28hI3zU1MhthpaGxi1D3fizwoFCPMn3eSlX-8en4OwdCw"; // ضع رابط Webhook الخاص بك هنا
     const message = document.getElementById("message").value;
 
     if (message.trim() === "") {
